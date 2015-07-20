59209bf83f05ac388a1a11f1ba633b1719fcc2ac
joseaps <joseaps@users.noreply.github.com>
2015-07-20 21:14:43 +0200

    Página Autor

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res) {
+  res.render('author', { autor: 'joseaps' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
