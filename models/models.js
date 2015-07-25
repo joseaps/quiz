f76045df573f6fa74948ecab8036c4e9931fa206
joseaps <joseaps@users.noreply.github.com>
2015-07-25 20:28:43 +0200

    Despliegue DB en Heruku

--- models/models.js
+++ models/models.js
@@ -1,11 +1,29 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-	{dialect: "sqlite", storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{ dialect:  protocol,
+	  protocol: protocol,
+	  port:     port,
+	  host:     host,
+	  storage:  storage, // solo SQLLite (.env)
+	  omitNull: true     // solo Postgres
+	}
 	);
 
 // Importar la definición de la tabla Quiz en quiz.js
