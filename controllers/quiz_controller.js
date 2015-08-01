b7ba0bee1e47e41656b2d01f2b4aae88d00023f5
joseaps <joseaps@users.noreply.github.com>
2015-08-01 14:16:38 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,7 +14,13 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
+	var search = "%";
+	if (req.query) {
+		if (req.query.search) {
+			search = "%" + req.query.search.replace(' ', '%') + "%";
+		}
+	}
+	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
 		res.render('quizes/index.ejs', { quizes: quizes});
 	}).catch(function(error) { next(error); });
 };
