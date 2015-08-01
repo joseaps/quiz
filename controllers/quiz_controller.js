22ccc42ec9ea1f1d47d0e30d4eeec3fe4f4f7f1c
joseaps <joseaps@users.noreply.github.com>
2015-08-01 21:06:16 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -63,3 +63,32 @@ exports.create = function(req, res) {
 		}
 	});
 };
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+	
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz
+	.validate()
+	.then(function(err) {
+		if (err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}
+		else {
+			req.quiz // save: guarda campos pregunta y respuesta en DB
+			.save( {fields: ["pregunta", "respuesta"]})
+			.then( function() {
+				res.redirect('/quizes');
+				// Redirección HTTP a lista de preguntas (URL relativo)
+			});
+		}
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,8 @@ router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
-router.post('/quizes/create',               quizController.create);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 module.exports = router;
