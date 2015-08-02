2b0e5538e128a8b3e6318477da138e825acf3b98
joseaps <joseaps@users.noreply.github.com>
2015-08-02 19:46:02 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -92,3 +92,10 @@ exports.update = function(req, res) {
 		}
 	});
 };
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
