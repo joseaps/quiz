db909938d833cc82cf9848a3137ed03a49ffb98a
joseaps <joseaps@users.noreply.github.com>
2015-08-08 19:32:37 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,13 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+	if (req.session.user) {
+		next();
+	}
+	else {
+		res.redirect('/login');
+	}
+};
+
 // GET /login   -- Formulario de login
 exports.new = function(req, res) {
 	var errors = req.session.errors || {};
