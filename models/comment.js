d9ab9194f23ddcc0294323c73a7ed811998c04d3
joseaps <joseaps@users.noreply.github.com>
2015-08-08 11:13:09 +0200

    Crear comentario

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+// Definición del modelo de Comment con validación
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define(
+		'Comment',
+		{ texto: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: { msg: "-> Falta Comentario"}}
+			}
+		}
+	);
+}
